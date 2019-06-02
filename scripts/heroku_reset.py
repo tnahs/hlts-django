bc5d25c260550cde09f1eaddbcf6dcffd8edee61
tnahs <31777797+tnahs@users.noreply.github.com>
2019-06-02 02:27:38 -0700

    Update heroku_reset.py

--- scripts/heroku_reset.py
+++ scripts/heroku_reset.py
@@ -6,27 +6,30 @@ import sys
 
 def main():
 
-    confirm = input("Confirm to hard reset application? [y/N]: ")
+    confirm = input("Confirm to hard reset heroku application? [y/N]: ")
 
     if confirm.lower().strip() != "y":
         print("Confirmation cancelled.")
         sys.exit(-1)
 
-    os.system("pg:reset DATABASE_URL --confirm hlts-django --app hlts-django")
+    os.system("heroku pg:reset DATABASE_URL --confirm hlts-django --app hlts-django")
 
-    os.system("python manage.py migrate \
-            --settings=hlts.settings.prod \
-            --app hlts-django")
+    os.system("heroku run \
+              python manage.py migrate \
+              --settings=hlts.settings.prod \
+              --app hlts-django")
 
-    os.system("python manage.py loaddata \
-            fixtures/init_defaults.json \
-            --settings=hlts.settings.prod \
-            --app hlts-django")
+    os.system("heroku run \
+              python manage.py loaddata \
+              fixtures/app_defaults.json \
+              --settings=hlts.settings.prod \
+              --app hlts-django")
 
-    os.system("python manage.py loaddata \
-            fixtures/prod_defaults.json \
-            --settings=hlts.settings.prod \
-            --app hlts-django")
+    os.system("heroku run \
+              python manage.py loaddata \
+              fixtures/dev_defaults.json \
+              --settings=hlts.settings.prod \
+              --app hlts-django")
 
 
 if __name__ == "__main__":
