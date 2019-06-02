1fa8ed1cbbe652d72603e074a15dd1c8557d817f
tnahs <31777797+tnahs@users.noreply.github.com>
2019-06-02 01:53:51 -0700

    Update heroku_reset.py

--- scripts/heroku_reset.py
+++ scripts/heroku_reset.py
@@ -12,21 +12,18 @@ def main():
         print("Confirmation cancelled.")
         sys.exit(-1)
 
-    os.system("heroku pg:reset DATABASE_URL --confirm hlts-django --app hlts-django")
+    os.system("pg:reset DATABASE_URL --confirm hlts-django --app hlts-django")
 
-    os.system("heroku run \
-            python manage.py migrate \
+    os.system("python manage.py migrate \
             --settings=hlts.settings.prod \
             --app hlts-django")
 
-    os.system("heroku run \
-            python manage.py loaddata \
+    os.system("python manage.py loaddata \
             fixtures/init_defaults.json \
             --settings=hlts.settings.prod \
             --app hlts-django")
 
-    os.system("heroku run \
-            python manage.py loaddata \
+    os.system("python manage.py loaddata \
             fixtures/prod_defaults.json \
             --settings=hlts.settings.prod \
             --app hlts-django")
