46868ac8d82d4bb61d515fa6e376b57fdd303c08
tnahs <31777797+tnahs@users.noreply.github.com>
2019-06-11 22:08:34 -0700

    Heroku test

--- config/settings/base.py
+++ config/settings/base.py
@@ -6,11 +6,11 @@ import pathlib
 SITE_ROOT = pathlib.Path(__file__).parent.parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
-STATIC_ROOT = SITE_ROOT / "staticfiles"
+STATIC_ROOT = SITE_ROOT / "static"
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [
-    SITE_ROOT / "static",
-]
+# STATICFILES_DIRS = [
+#     SITE_ROOT / "static",
+# ]
 
 MEDIA_ROOT = SITE_ROOT / "media"
 MEDIA_URL = "/media/"
